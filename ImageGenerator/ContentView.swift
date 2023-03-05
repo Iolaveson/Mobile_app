b593c774518fba8f331341c7dee696be0a196f35
Israel Olaveson <97457622+Iolaveson@users.noreply.github.com>
2023-03-05 10:32:40 -0800

    Add files via upload

---
+++ ImageGenerator/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  ImageGenerator
+//
+//  Created by user236073 on 3/5/23.
+//
+
+import OpenAIKit
+import SwiftUI
+
+final class ViewModel: ObservableObject {
+    private var openai: OpenAI?
+    
+    func setup() {
+        openai = OpenAI(Configuration(
+            organizationId: "Personal",
+            apiKey: "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"
+        ))
+    }
+    
+    func generateImage (prompt: String) async -> UIImage? {
+        guard let openai = openai else {
+            return nil
+        }
+        
+        do {
+            let params = ImageParameters(
+                prompt: prompt,
+                resolution: .medium,
+                responseFormat: .base64Json
+            )
+            let result = try await openai.createImage(
+                parameters: params
+            )
+            let data = result.data[0].image
+            let image = try openai.decodeBase64Image(data)
+            return image
+        }
+        catch {
+            print(String(describing: error))
+            return nil
+        }
+    }
+}
+
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var image: UIImage?
+    
+    
+    var body: some View {
+        NavigationView{
+            VStack {
+                Spacer()
+                if let image = image{
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250, height: 250)
+                }
+                else{
+                    Text("Type a prompt to generate image")
+                }
+                Spacer()
+                
+                TextField("Type prompt here...", text: $text)
+                    .padding()
+                Button("Generate Image"){
+                    if !text.trimmingCharacters(in: .whitespaces).isEmpty{
+                        Task{
+                            let result = await viewModel.generateImage(prompt: text)
+                            if result == nil{
+                                print("Failed to get image")
+                            }
+                            self.image = result
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("AI Image Generator")
+            .onAppear{
+                viewModel.setup()
+            }
+            .padding()
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by user236073 on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
